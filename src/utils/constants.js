9833a35f50d5a8516fbb10beeaa0d6721fd3c119
Daniel Khrenov <klaven011@gmail.com>
2021-07-04 12:50:47 +0300

    chore: fix api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const beatfilmAPI = "https://api.nomoreparties.co/beatfilm-movies";
-const filmAPI = "https://api.fl0ppat-diplom.nomoredomains.club/";
+const filmAPI = "https://api.fl0ppat-diplom.nomoredomains.club";
 const imageLink = "https://api.nomoreparties.co";
 const internailError =
   "Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз";
