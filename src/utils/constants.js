06d5d902ab5922310f2c54ca1e59ba3e66503a21
Daniel Khrenov <klaven011@gmail.com>
2021-07-04 11:58:09 +0300

    feat: add constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 const beatfilmAPI = "https://api.nomoreparties.co/beatfilm-movies";
-const filmAPI = "";
+const filmAPI = "http://localhost:3000";
+const imageLink = "https://api.nomoreparties.co";
 const internailError =
   "Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз";
 
-export default { beatfilmAPI, filmAPI, internailError };
+export default { beatfilmAPI, filmAPI, internailError, imageLink };
