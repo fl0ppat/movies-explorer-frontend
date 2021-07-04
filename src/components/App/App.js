25d263a2338a426d11e1690f53b204a79e59329d
Daniel Khrenov <klaven011@gmail.com>
2021-07-04 12:01:37 +0300

    feat: main release update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,63 +1,120 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Route, Switch, HashRouter } from "react-router-dom";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Movies from "../Movies/Movies";
-import SavedMovies from "../SavedMovies/SavedMovies";
 import Register from "../Register/Register";
 import Login from "../Login/Login";
 import Profile from "../Profile/Profile";
 import NotFound from "../NotFound/NotFound";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 import Footer from "../Footer/Footer";
 
-// eslint-disable-next-line no-unused-vars
+import api from "../../utils/AuthApi";
+
 import UserContext from "../../context/userContext";
+import { ModalProvider } from "../../context/modalContext";
 
 import "./App.css";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
-
-  function debugUserSet(state) {
-    setCurrentUser(state);
-  }
+  const [loggedIn, setLoggedIn] = useState(window.localStorage.getItem("token"));
 
   useEffect(() => {
-    debugUserSet(1);
+    if (loggedIn) {
+      api
+        .getUser()
+        .then((data) => {
+          setCurrentUser(data);
+        })
+        .catch((err) => console.error(err));
+    }
   }, []);
+
+  function onLogin(token) {
+    setLoggedIn(token);
+    window.localStorage.setItem("token", token);
+    api
+      .getUser()
+      .then((data) => {
+        setCurrentUser(data);
+      })
+      .catch((err) => console.error(err));
+  }
+
+  function onChangeProfile(name, email) {
+    return api
+      .editProfileData(name, email)
+      .then((data) => {
+        setCurrentUser(data);
+      })
+      .catch((err) => console.error(err));
+  }
+
+  function likeFilm(id) {
+    if (!currentUser.movies.includes(id)) {
+      const newCurrentUser = currentUser;
+      newCurrentUser.movies.push(id);
+      setCurrentUser(newCurrentUser);
+    }
+  }
+
+  function dislikeFilm(id) {
+    if (currentUser.movies.includes(id)) {
+      const newCurrentUser = currentUser;
+      newCurrentUser.movies.filter((item) => item !== id);
+      setCurrentUser(newCurrentUser);
+      return true;
+    }
+  }
+
+  function onSignOut() {
+    api
+      .signOut()
+      .then(() => {
+        setLoggedIn(null);
+        window.localStorage.removeItem("token");
+        setCurrentUser(null);
+      })
+      .catch((err) => console.error(err));
+  }
 
   return (
     <UserContext.Provider value={currentUser}>
-      <div className='App'>
-        <HashRouter basename='/'>
-          <Header />
+      <ModalProvider>
+        <div className='App'>
+          <HashRouter basename='/'>
+            <Header loggedIn={loggedIn} />
 
-          <Switch>
-            <Route path='/movies'>
-              <Movies />
-            </Route>
-            <Route path='/saved-movies'>
-              <SavedMovies />
-            </Route>
-            <Route path='/signup'>
-              <Register />
-            </Route>
-            <Route path='/signin'>
-              <Login />
-            </Route>
-            <Route path='/profile'>
-              <Profile />
-            </Route>
-            <Route exact path='/'>
-              <Main />
-            </Route>
-            <Route>
-              <NotFound />
-            </Route>
-          </Switch>
-          <Footer />
-        </HashRouter>
-      </div>
+            <Switch>
+              <ProtectedRoute path='/movies' loggedIn={loggedIn} component={Movies} onLike={likeFilm} />
+              <ProtectedRoute path='/saved-movies' loggedIn={loggedIn} component={Movies} onDislike={dislikeFilm} />
+              <Route path='/signup'>
+                <Register onLogin={onLogin} />
+              </Route>
+              <Route path='/signin'>
+                <Login onLogin={onLogin} />
+              </Route>
+              <ProtectedRoute
+                path='/profile'
+                loggedIn={loggedIn}
+                onSignOut={onSignOut}
+                onChangeProfile={onChangeProfile}
+                currentUser={currentUser}
+                component={Profile}
+              />
+              <Route exact path='/'>
+                <Main />
+              </Route>
+              <Route>
+                <NotFound />
+              </Route>
+            </Switch>
+            <Footer />
+          </HashRouter>
+        </div>
+      </ModalProvider>
     </UserContext.Provider>
   );
 }
